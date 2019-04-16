c1496fc102f5f64ca8f1e7911bb855d55212a862
Quan Le <lequan1@myvuw.ac.nz>
2019-04-17 10:15:55 +1200

    learning about states

--- src/index.js
+++ src/index.js
@@ -2,16 +2,26 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
-
 class Square extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+		value: null,
+	};
+  }
+
   render() {
-    return <button className="square">{/* TODO */}</button>;
+    return (
+      <button className="square" onClick={() => alert("click")}>
+        {this.props.value}
+      </button>
+    );
   }
 }
 
 class Board extends React.Component {
   renderSquare(i) {
-    return <Square />;
+    return <Square value={i} />;
   }
 
   render() {
@@ -57,7 +67,7 @@ class Game extends React.Component {
 }
 
 class Test extends React.Component {
-	//test add for git setup
+  //test add for git setup
 }
 // ========================================
 
